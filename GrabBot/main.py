e75369f1b9c7dc05db1226ff3b2838d07629e82b
jackamsbry <jamsbry1@gmail.com>
2020-01-28 15:37:32 -0500

    added manual control and search algorithm (WIP)

--- GrabBot/main.py
+++ GrabBot/main.py
@@ -7,11 +7,10 @@ from pybricks.parameters import (Port, Stop, Direction, Button, Color,
                                  SoundFile, ImageFile, Align)
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
+from passwords import Key as Key
 
 import ubinascii, ujson, urequests, utime
-     
-Key = 'w-TANbEa0fhalpKtCPNGF0Zuuc6J3r9st6-FSUv55v'
-     
+
 def SL_setup():
      urlBase = "https://api.systemlinkcloud.com/nitag/v2/tags/"
      headers = {"Accept":"application/json","x-ni-api-key":Key}
@@ -50,7 +49,6 @@ def Create_SL(Tag, Type):
      except Exception as e:
           print(e)
 
-
 ultrasonic = UltrasonicSensor(Port.S1)
 grabMotor = Motor(Port.B)
 
@@ -63,54 +61,155 @@ wheel_spacing = 164
 driveBase = DriveBase(l_motor, r_motor, wheel_dia, wheel_spacing)
 
 grabMotor.dc(0)
-wasGrab = False
-Put_SL('isGrab','BOOLEAN', 'false')
+wasGrab = True
+isGrab = True
+Put_SL('isGrab','BOOLEAN', 'true')
 
-dist = 200
+
+init_search_dir = Get_SL('SearchDirection')
+dist_thresh = 150
+turn_thresh = 200
+count = 0
+best_dist = 5000
+time_prev = 0
+
+watch = StopWatch()
+watch.pause()
+watch.reset()
 
 while True :
-     isGrab = Get_SL('isGrab')
-     isGrab = True if isGrab == "true" else False
-     isSearch = Get_SL('isSearch')
-     isSearch = True if isSearch == "true" else False
-     driveCMD = Get_SL('driveCMD')
-     if isGrab == True and wasGrab == False:
-          grabMotor.dc(100)
-          wait(2000)
-          grabMotor.dc(0)
-          wasGrab = isGrab
-     
-     if isGrab == False and wasGrab == True:
-          grabMotor.dc(-100)
-          wait(2000)
-          grabMotor.dc(0)
-          wasGrab = isGrab
+     isManual = Get_SL('ManualCtl')
+     isManual = True if isManual == "true" else False
+     if isManual:
+          isGrab = Get_SL('isGrab')
+          isGrab = True if isGrab == "true" else False
+          driveCMD = Get_SL('driveCMD')
+          if isGrab == True and wasGrab == False:
+               grabMotor.dc(100)
+               wait(2000)
+               grabMotor.dc(0)
+               wasGrab = isGrab
+          
+          if isGrab == False and wasGrab == True:
+               grabMotor.dc(-100)
+               wait(2000)
+               grabMotor.dc(0)
+               wasGrab = isGrab
 
-     kp = float(Get_SL('kp'))
-     ultDist = ultrasonic.distance()
-     print(ultDist)
-     error = ultDist - dist
-    
-     speed = kp * error
-
-     if driveCMD == '0' :
-          driveBase.stop(Stop.BRAKE)
-     elif driveCMD == '1' :
-          driveBase.drive(speed,0)
-     elif driveCMD == '2' :
-          driveBase.drive(10,45)
-     elif driveCMD == '3' :
-          driveBase.drive(10,-45)
-     elif driveCMD == '4' :
-          driveBase.drive(-100,0)
-
-     if isSearch == True :
-          driveBase.drive(0, 30)
           ultDist = ultrasonic.distance()
-          print('Searching for Object')
-          if ultDist < 500 :
-               print('Found!')
-               isSearch = False
-               Put_SL('isSearch','BOOLEAN','false')
-               Put_SL('driveCMD','NUMBER','1')
-               
+
+          if driveCMD == '0' :
+               driveBase.stop(Stop.BRAKE)
+          elif driveCMD == '1' :
+               driveBase.drive(100,0)
+          elif driveCMD == '2' :
+               driveBase.drive(10,45)
+          elif driveCMD == '3' :
+               driveBase.drive(10,-45)
+          elif driveCMD == '4' :
+               driveBase.drive(-100,0)
+     else:
+          ultDist = ultrasonic.distance() #Get distance from ultrasonic sensor
+          if ultDist > dist_thresh:
+               watch.resume()
+               if watch.time() <= 1500:
+                    driveBase.drive(100, 0)
+               else:
+                    if init_search_dir == '0':
+                         turn_direction = -70
+                         speed = 0
+                    elif init_search_dir == '1':
+                         turn_direction = 70
+                         speed = 0
+                    else:
+                         turn_direction = 20
+                         speed = 50
+                    driveBase.drive(speed, turn_direction)
+                    ultDist = ultrasonic.distance()
+                    if ultDist < best_dist:
+                         best_dist = ultDist
+                         time1 = watch.time()
+                         time_dif = time1 - time_prev
+                         print(time_dif)
+                    time_prev = time1     
+                    if time_dif > 6000:
+                              watch.pause()
+                              watch.reset()
+                              time_prev = 0
+
+                    
+
+
+                    
+                    
+
+
+
+
+          
+          
+          
+          
+          
+          # ultDist = ultrasonic.distance()
+          # print(ultDist)
+          # #print(turn_thresh)
+          # if isTurn:
+          # #Sweep search to minimize err in initial search direction only for first loop
+          #      if count == 0:
+          #           if init_search_dir == '0':
+          #                driveBase.drive_time(0,-45,1500)
+          #           elif init_search_dir == '1':
+          #                driveBase.drive_time(0,45,1500)
+          #           else:
+          #                pass
+          #      print('Sweeping')
+          #      while watch.time() < 2000:
+          #           driveBase.drive(0,45)
+          #           watch.resume()
+          #           ultDist = ultrasonic.distance()
+          #           if ultDist < best_dist:
+          #                best_dist = ultDist
+          #                break
+          #      watch.pause()
+          #      watch.reset()
+
+          #      while watch.time() < 4000:
+          #           driveBase.drive(0,-45)
+          #           watch.resume()
+          #           ultDist = ultrasonic.distance()
+          #           if ultDist <= best_dist:
+          #                best_dist = ultDist
+          #                break
+          #      watch.pause()
+          #      watch.reset()
+
+          #      while watch.time() < 4000:
+          #           driveBase.drive(0,45)
+          #           watch.resume()
+          #           ultDist = ultrasonic.distance()
+          #           if ultDist <= best_dist:
+          #                best_dist = ultDist
+          #                break
+                         
+          #      watch.pause()
+          #      watch.reset()
+          #      print('Best Distance is {}'.format(best_dist))
+          #      count += 1
+          #      turn_thresh *= 0.99
+          #      isTurn = False
+
+          # if best_dist > dist_thresh or ultDist > dist_thresh:
+          #      watch.resume()
+          #      ultDist = ultrasonic.distance()
+          #      kp = 0.9
+          #      err = ultDist - dist_thresh
+          #      print('Error is {}'.format(err))
+          #      speed = kp * err
+          #      print('Driving')
+          #      driveBase.drive(speed, 0)
+          #      if watch.time() >= 1500:
+          #           watch.pause()
+          #           watch.reset()
+          #           isTurn = True
+          
---
+++ GrabBot/passwords.py
@@ -0,0 +1,4 @@
+
+
+#Systemlink API key
+Key = 'w-TANbEa0fhalpKtCPNGF0Zuuc6J3r9st6-FSUv55v'
