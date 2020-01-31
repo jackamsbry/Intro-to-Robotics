57c1b6339c47be9fcc78b8eb0f6237d4a677a6fb
jackamsbry <jamsbry1@gmail.com>
2020-01-31 12:50:19 -0500

    come small things

--- Line_Follow/main.py
+++ Line_Follow/main.py
@@ -10,36 +10,34 @@ from pybricks.robotics import DriveBase
 
 from pybricks.ev3devio import Ev3devSensor 
 import utime
+import uio
 import ev3dev2
-import ev3Sensor
+from ev3Sensor import EV3Sensor
 from ev3dev2.port import LegoPort
 
 # Write your program here
-brick.sound.beep()
+
 
 left = Motor(Port.D)
 right = Motor(Port.A)
 
-ultrasonic = UltrasonicSensor(Port.S1)
+ultrasonic = UltrasonicSensor(Port.S2)
 
 dist_thresh = 100
 
 filename = './log.txt'
-fin = uio.open(filename, )
+f 
 
-def grayscale():
-    
+def main():
+    brick.sound.beep()
+    sens = LegoPort(address ='ev3-ports:in1') # which port?? 1,2,3, or 4
+    sens.mode = 'ev3-analog'
+    # sens2 = LegoPort(address ='ev3-ports:in4')
+    # sens2.mode = 'ev3-analog'
+    utime.sleep(0.5)
+    sensor_left = EV3Sensor(Port.S1) # same port as above
+    while True:
+        print(sensor_left.read())
+        wait(200)
 
-
-if __name__=="main":
-    def main():
-        brick.sound.beep()
-        sens = LegoPort(address ='ev3-ports:in1') # which port?? 1,2,3, or 4
-        sens.mode = 'ev3-analog'
-        utime.sleep(0.5)
-        sensor_left=ev3Sensor(Port.S1) # same port as above
-        while True:
-            print(sensor_left.readvalue())
-            wait(200)
-
-    main()
+main()
