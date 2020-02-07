191ac45f872689642a15910e472e09d303c9b125
jackamsbry <jamsbry1@gmail.com>
2020-02-07 18:41:14 -0500

    initial commit

---
+++ Images_4_blind/Scanner/main.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color,
+                                 SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
+from passwords import Key
+
+import ubinascii, ujson, utime, urequests
+
+
+
+def SL_setup():
+     urlBase = "https://api.systemlinkcloud.com/nitag/v2/tags/"
+     headers = {"Accept":"application/json","x-ni-api-key":Key}
+     return urlBase, headers
+     
+def Put_SL(Tag, Type, Value):
+     urlBase, headers = SL_setup()
+     urlValue = urlBase + Tag + "/values/current"
+     propValue = {"value":{"type":Type,"value":Value}}
+     try:
+          reply = urequests.put(urlValue,headers=headers,json=propValue).text
+     except Exception as e:
+          print(e)         
+          reply = 'failed'
+     return reply
+
+def Get_SL(Tag):
+     urlBase, headers = SL_setup()
+     urlValue = urlBase + Tag + "/values/current"
+     try:
+          value = urequests.get(urlValue,headers=headers).text
+          data = ujson.loads(value)
+          #print(data)
+          result = data.get("value").get("value")
+     except Exception as e:
+          print(e)
+          result = 'failed'
+     return result
+     
+def Create_SL(Tag, Type):
+     urlBase, headers = SL_setup()
+     urlTag = urlBase + Tag
+     propName={"type":Type,"path":Tag}
+     try:
+          urequests.put(urlTag,headers=headers,json=propName).text
+     except Exception as e:
+          print(e)
+# Write your program here
+
+
+left = Motor(Port.A)
+right = Motor(Port.D)
+light = ColorSensor(Port.S1)
+
+robot = DriveBase(left, right, 56, 152)
+
+
+while True:
+     reflection = light.reflection()
+     color = light.color()
+     
+     if reflection < 70:
+          Put_SL('line','BOOLEAN','true')
+     else: 
+          Put_SL('line','BOOLEAN','false')
+
+     #robot.drive(0,10)
+
