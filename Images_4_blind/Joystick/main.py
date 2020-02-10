4ce1ce979df30f79f9746c3e76a28034455e71e4
jackamsbry <jamsbry1@gmail.com>
2020-02-09 23:12:09 -0500

    working now

--- Images_4_blind/Joystick/main.py
+++ Images_4_blind/Joystick/main.py
@@ -9,25 +9,21 @@ from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 from math import *
 
-import utime
+import utime, ujson
 from pybricks.iodevices import UARTDevice
 
 
 
 def moveEncode(x, y):
-    x = int(x)
-    y = int(y)
-
-    moveCMD = (x>>4) | (y<<4)
-    moveCMD = bytes(moveCMD)
-    return moveCMD
+    coords = {'x':x, 'y':y}
+    moveCMD =  ujson.dumps(coords)
+    wait(100)
+    return moveCMD + ''.join(['B' for i in range(32-len(moveCMD))])
 
 def constrain(number, range):
     if number > range[1]:
-        print(number)
         number = range[1]
     elif number < range[0]:
-        print(number)
         number = range[0]
     return number
 
@@ -40,37 +36,39 @@ def main():
     uartComm = UARTDevice(Port.S2, 9600)
 
     while True:
+        
         #initial angle reading (correcting signs of angles)
         x = - xMotor.angle()
         y = - yMotor.angle()
-        x += 90
-        y += 90
-        #convert x and y values to a number between 0 and 127
-        x *= (128/180)
-        y *= (128/180)
-        #constrain numbers to be within range of an 4 bit number
-        x_scale = [0, 127]
-        y_scale = [0, 127]
-        x = constrain(x, x_scale)
-        y = constrain(y, y_scale)
+        # x += 90
+        # y += 90
+        # #convert x and y values to a number between 0 and 127
+        # x *= (256/180)
+        # y *= (256/180)
+        # x = int(x)
+        # y = int(y)
+        # #constrain numbers to be within range of an 4 bit number
+        # x_scale = [0, 255]
+        # y_scale = [0, 255]
+        # x = constrain(x, x_scale)
+        # y = constrain(y, y_scale)
+
         #convert x and y to single 8 bit number
         moveCMD = moveEncode(x, y)
 
         if uartComm.waiting() > 0:
-            uartComm.read()
-        
-        uartComm.write(moveCMD) 
+            data_raw = uartComm.read()
+        try:
+            uartComm.write(moveCMD) 
+        except:
+            print("Write Failed")
 
         if resetAngle.pressed():
             xMotor.reset_angle(0)
             yMotor.reset_angle(0)
 
-        if not joyMove.pressed():
-            xMotor.stop(Stop.HOLD)
-            yMotor.stop(Stop.HOLD)
-        else:
-            xMotor.stop(Stop.COAST)
-            yMotor.stop(Stop.COAST)
+        xMotor.stop(Stop.COAST)
+        yMotor.stop(Stop.COAST)
 
 main()
         
