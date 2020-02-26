690382e47517112fc0c44509a19a7f564c9e1a5c
jackamsbry <60232817+jackamsbry@users.noreply.github.com>
2020-02-25 20:52:17 -0500

    added queue for bricks

--- LegoSorter/main.py
+++ LegoSorter/main.py
@@ -7,10 +7,6 @@ from pybricks.tools import wait, StopWatch
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 from pybricks.iodevices import UARTDevice
-import serial
-
-from ucollections import deque
-
 
 # Write your program here
 ev3 = EV3Brick()
@@ -22,9 +18,9 @@ bucketMotor = Motor(Port.B)
 light = ColorSensor(Port.S1)
 
 uart = UARTDevice(Port.S4, 115200, timeout=0)
-time = Stopwatch()
+time = StopWatch()
 #Set up a queue for bricks on the conveyor belt
-brickQueue = deque((),maxlen=5)
+brickQueue = []
 
 count = 0
 reflectMean = 0
@@ -34,10 +30,11 @@ meanSize = 10
 numStep = 0
 redPos = 0
 trashPos = 180 
+currPos = 0
 
-bucketMotor.reset(0)
-time.pause()
+bucketMotor.reset_angle(0)
 time.reset()
+time.resume()
 
 
 while True:
@@ -51,7 +48,7 @@ while True:
         # Average every 10 readings
         reflectMean = reflectMeanNum / meanSize
         print(reflectMean)
-        if reflectMean > 0.5 :
+        if reflectMean > 0.5:
             print("Brick Detected")
             if wasBrick == 0:
                 sendData = bytes('1', 'utf-8')
@@ -67,22 +64,29 @@ while True:
 
     # Try to read from serial port
     try:
-        rawData = uart.read(1).decode('utf-8')
-        isRed = True if rawData == '1' else False
+        getData = uart.read(1).decode('utf-8')
+        print(getData)
+        isRed = True if getData == "b'0'" else False
 
     except Exception as errMsg:
-        print("Read Failed")
+        #print("Read Failed")
         print(errMsg)
 
-
     try:
-        if (time.time() > (brickQueue.popleft() + 10000):
+        if (time.time() > (brickQueue[0] + 10000)):
+            brickQueue.pop(0)
             if isRed == True:
-                bucketMotor.run_target(50, redPos)
+                if currPos == 180:
+                    bucketMotor.run_target(50, redPos)
+                    currPos = redPos
+                else:
+                    pass
             else:
-                bucketMotor.run_target(50, trashPos)
-        else:
-            print('Wait for Brick to arrive')
+                if currPos == 0:
+                    bucketMotor.run_target(50, trashPos)
+                    currPos = trashPos
+                else:
+                    pass
     except:
         print('Nothing in Queue')
 
