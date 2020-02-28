842dc4d9bcc91f0fb3e2a23ca8215dd9b021e76e
jackamsbry <60232817+jackamsbry@users.noreply.github.com>
2020-02-28 16:56:31 -0500

    initial commit

---
+++ ball_thrower/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+# Write your program here
+ev3 = EV3Brick()
+ev3.speaker.beep()
